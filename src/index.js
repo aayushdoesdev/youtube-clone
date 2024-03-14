c47cfc23c248aa8c223ba3b20d47963f68d303db
Aayush Vaishnav <aayushdoesdev@gmail.com>
2024-03-14 11:02:03 +0530

    created utils to handle api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on Port: ${process.env.PORT}`)
+    }) 
+})
+.catch((err) => console.error("Connection to DB Failed", err))
