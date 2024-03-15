e58a68dd53310d4306b0fc5f578bf178cadb0b35
Aayush Vaishnav <aayushdoesdev@gmail.com>
2024-03-15 10:44:36 +0530

    cloudinary and multer config done

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+ export const upload = multer({ storage: storage }) // storage can be removed from here if there are same words in ES6
